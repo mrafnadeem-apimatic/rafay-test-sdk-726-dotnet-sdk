c919d2ea797ffb597c8a734bdf97e0ea8c1f425c
mrafnadeem-apimatic <muhammad.nadeem@apimatic.io>
2026-08-10 10:30:35 +0000

    Automated commit message

--- SwaggerPetstoreOpenApi30.Standard/SwaggerPetstoreOpenApi30Client.cs
+++ SwaggerPetstoreOpenApi30.Standard/SwaggerPetstoreOpenApi30Client.cs
@@ -38,7 +38,7 @@ namespace SwaggerPetstoreOpenApi30.Standard
 
         private readonly GlobalConfiguration globalConfiguration;
         private SdkLoggingConfiguration sdkLoggingConfiguration;
-        private const string userAgent = "DotNet-SDK/1.0.1 (OS: {os-info}, Engine: {engine}/{engine-version})";
+        private const string userAgent = "DotNet-SDK/1.0.2 (OS: {os-info}, Engine: {engine}/{engine-version})";
         private readonly HttpCallback httpCallback;
         private readonly Lazy<PetApi> pet;
         private readonly Lazy<StoreApi> store;
